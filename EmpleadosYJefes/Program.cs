4f40329708fcb74cc8380f1072e3aaccc19317bd
Marco Huerta <marco.huerta.sc@pm.me>
2021-08-10 16:12:05 -0500

    Inicializando Solución

---
+++ EmpleadosYJefes/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace EmpleadosYJefes
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
