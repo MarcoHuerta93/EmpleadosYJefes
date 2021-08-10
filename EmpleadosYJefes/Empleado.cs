9533b23332dddf56e6c5fe1e9e10565f4501566a
Marco Huerta <marco.huerta.sc@pm.me>
2021-08-10 16:30:57 -0500

    Creación de clase Empleado con propiedades y constructores

---
+++ EmpleadosYJefes/Empleado.cs
@@ -0,0 +1,30 @@
+﻿using System;
+namespace EmpleadosYJefes
+{
+    class Empleado
+    {
+
+        //Propiedades
+        public string Nombre { get; set; }
+        public string Apellido { get; set; }
+        public int Salario { get; set; }
+
+        //Constructores
+        public Empleado()
+        {
+            Nombre = "Marco";
+            Apellido = "Huerta";
+            Salario = 20000;
+        }
+
+        //Constructor parametrizado
+
+        public Empleado(string nombre, string apellido, int salario)
+        {
+            this.Nombre = nombre;
+            this.Apellido = apellido;
+            this.Salario = salario;
+        }
+        
+    }
+}
